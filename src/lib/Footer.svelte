5022a35db4d4d852d2c5b7a592c5fbe01223d4a6
Chaz <chaz.mateer@gmail.com>
2023-04-25 22:20:15 -0400

    Couple styling tweaks.

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,3 +1,14 @@
-<footer>
-  Footer
+<footer class="footer">
+	<span class="text-xs p-1">0.0.1</span>
 </footer>
+
+<style>
+	.footer {
+		background-image: radial-gradient(
+				at 0% 0%,
+				rgba(var(--color-secondary-500) / 0.33) 0px,
+				transparent 50%
+			),
+			radial-gradient(at 98% 1%, rgba(var(--color-error-500) / 0.33) 0px, transparent 50%);
+	}
+</style>
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,21 +1,43 @@
-<script>
-	import { AppBar } from '@skeletonlabs/skeleton';
+<script lang="ts">
+	import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+	// import { setContext } from 'svelte';
+	// import { writable } from 'svelte/store';
+
+	// // import type { LayoutData } from './$types';
+
+	// export let data: any;
+	// // Create a store and update it when necessary...
+	// const darkMode = writable();
+	// $: darkMode.set(data.darkMode);
+	// // ...and add it to the context for child components to access
+	// setContext('darkMode', darkMode);
 </script>
 
-<header>
+<header class="header">
 	<AppBar
 		gridColumns="grid-cols-3"
 		slotDefault="place-self-center"
 		slotTrail="place-content-end"
-		class="bg-gradient-to-tr from-bg-primary-500 to-pink-500 transform"
+		class="header"
 	>
 		<svelte:fragment slot="lead">🍔</svelte:fragment>
-		<h3>
+		<div class="text-lg md:text-3xl text-center">
 			🚌 <span
-				class="bg-gradient-to-br from-pink-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone font-extrabold"
+				class="permanent-marker bg-gradient-to-br from-pink-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone font-extrabold"
 				>RVA Bus Stuff</span
-			>
-		</h3>
-		<svelte:fragment slot="trail">(actions)</svelte:fragment>
+			>🍑
+		</div>
+		<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
 	</AppBar>
 </header>
+
+<style>
+	.header {
+		background-image: radial-gradient(
+				at 0% 0%,
+				rgba(var(--color-secondary-500) / 0.33) 0px,
+				transparent 50%
+			),
+			radial-gradient(at 98% 1%, rgba(var(--color-error-500) / 0.33) 0px, transparent 50%);
+	}
+</style>
